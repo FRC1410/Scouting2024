ac5a1d272154261985d2079bd972d4e3be40a3c8
Lew Parker <parker@earthvectors.com>
2024-01-15 23:38:23 -0700

    Attempt to fix controllers import on heroku

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,6 @@
 import {Application} from "@hotwired/stimulus"
 import "tom-select"
-import SelectController from "./select_controller";
+import SelectController from "controllers/select_controller";
 const application = Application.start()
 // Configure Stimulus development experience
 application.debug = false
