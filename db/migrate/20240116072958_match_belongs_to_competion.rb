19fe692b4568998cdb00b6c0685a67af6dddd242
Lew Parker <parker@earthvectors.com>
2024-01-16 01:45:35 -0700

    Fix for heroku

--- db/migrate/20240116072958_match_belongs_to_competion.rb
+++ db/migrate/20240116072958_match_belongs_to_competion.rb
@@ -1,7 +1,7 @@
 class MatchBelongsToCompetion < ActiveRecord::Migration[7.1]
   def change
     change_table :matches do |table|
-      table.references :competition, null: false, foreign_key: true, default: 1
+      table.references :competition, null: false, foreign_key: true, default: 11
     end
   end
 end
