6142c12fe2cae0272fc8f02bb4d03a1e0bbba437
Lew Parker <parker@earthvectors.com>
2024-01-19 01:02:45 -0700

    Fix bug with new team log entry

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -4,6 +4,8 @@ import SelectController from "controllers/select_controller";
 import InputController from "controllers/input_controller";
 import LogEditController from "controllers/log_edit_controller";
 import FormResetController from "controllers/form_reset_controller";
+import FoundationController from "controllers/foundation_controller";
+
 const application = Application.start()
 // Configure Stimulus development experience
 application.debug = false
@@ -15,4 +17,5 @@ application.register("select", SelectController);
 application.register("input", InputController);
 application.register("log_edit", LogEditController);
 application.register("form_reset", FormResetController);
+application.register("foundation", FoundationController);
 application.ApplicationState = {selected: []}
