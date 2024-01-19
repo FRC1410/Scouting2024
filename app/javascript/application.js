964ef7eb19a86325a3249a0186067a08a4c94d94
Lew Parker <parker@earthvectors.com>
2024-01-19 00:34:26 -0700

    Cleanup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,19 +4,13 @@ import "controllers"
 import jquery from "jquery"
 import "foundation-sites"
 import Rails from "@rails/ujs"
+
 Rails.start()
 
 window.jQuery = jquery
 window.$ = jquery
 
-$(function() {
+$(function () {
     $(document).foundation();
-    $(document).on("turbo:frame-load", () => {
-        console.log('try reinit')
-        Foundation.reInit('accordion');})
-    $(document).on("turbo:frame-missing", () => {
-        console.log('try reinit')
-        Foundation.reInit('accordion');})
-
     Turbo.session.drive = false
 })
